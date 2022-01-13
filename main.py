0e3ef8e3f61909066934dc41b6eeea78060bc624
Karolina Koza <>
2022-01-13 16:01:23 +0000

    Looped

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letters_amonut=len(alphabet)
+working=True
 
 def cipher(message, number, task):
   cipher_txt=''
@@ -10,15 +11,19 @@ def cipher(message, number, task):
       cipher_txt+=i
     else:
       location=alphabet.index(i)+number
-      if location>letters_amonut:
+      if location+1>letters_amonut:
         location=location%letters_amonut
       cipher_txt+=alphabet[location]
   print(cipher_txt)
 
+while working:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
+  cipher(text, shift, direction)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-cipher(text, shift, direction)
+  exit=input('Restart?(Y/N) ').upper()
+  if exit=='N':
+    working=False
+print("I hope I helped you.")
