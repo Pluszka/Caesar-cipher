f5b0535b8f9ef2d344baf227d10e50158d8dfe31
Karolina Koza <>
2022-01-13 13:55:19 +0000

    One function for encode and decode

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letters_amonut=len(alphabet)
 
-def encrypt(message, number):
+def cipher(message, number, task):
   cipher_txt=''
-  for i in message:
-    location=alphabet.index(i)+number
-    if location>letters_amonut:
-      location=location%letters_amonut
-    cipher_txt+=alphabet[location]
-  print(cipher_txt)
-
-def decrypt(message, number):
-  cipher_txt=''
-  for i in message:
-    location=alphabet.index(i)-number
-    cipher_txt+=alphabet[location]
+  if task=='encode':
+    for i in message:
+      location=alphabet.index(i)+number
+      if location>letters_amonut:
+        location=location%letters_amonut
+      cipher_txt+=alphabet[location]
+  elif task=='decode':
+    for i in message:
+      location=alphabet.index(i)-number
+      cipher_txt+=alphabet[location]
   print(cipher_txt)
 
 
@@ -23,23 +21,4 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction=='encode':
-  encrypt(text, shift)
-elif direction=='decode':
-  decrypt(text, shift)
-
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+cipher(text, shift, direction)
